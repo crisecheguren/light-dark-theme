f9f682f22aaa94ea18ccc50899612bd8e78c1fd3
Cris <crisecheguren@gmail.com>
2022-02-28 11:48:29 -0500

    code review

--- script.js
+++ script.js
@@ -12,35 +12,27 @@ function imageMode(color) {
     image3.src = `img/undraw_compose_music_${color}.svg`;
 }
 
-
-// Dark Mode Styles
-function darkMode() {
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)'
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)'
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    imageMode('dark')
+// toggle between light and dark mode
+function toggleDarkLightMode(isDark) {
+    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun') ;
+    isDark ? imageMode('dark') : imageMode('light')
 }
 
-// Light Mode Styles
-function lightMode() {
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)'
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)'
-    toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    imageMode('light')
-}
+
 
 // switch theme dynamically
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark')
-        darkMode();
+        toggleDarkLightMode(true);
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light')
-        lightMode();
+        toggleDarkLightMode(false);
     }
 }
 
@@ -54,6 +46,6 @@ if (currentTheme) {
 
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
-        darkMode();
+        toggleDarkLightMode(true);
     }
 }
